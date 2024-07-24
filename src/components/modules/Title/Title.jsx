6b8d2a381d4261b0974c997836bf2673de0be5c6
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-24 14:19:22 +0330

    created section title for all

---
+++ src/components/modules/Title/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaStar } from 'react-icons/fa'
+
+function Title({title}) {
+  return (
+    <div className="inline">
+            <div className="inline-flex items-center border border-font-100 p-3 rounded-lg">
+                <span className="text-2xl"><FaStar/></span>
+                <span className="block font-danaBold text-2xl mr-3">{title}</span>
+            </div>
+        </div>
+  )
+}
+
+export default Title
