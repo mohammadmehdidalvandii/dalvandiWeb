34e1099e385f492a76a38aa7a0585a176ea18b03
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-27 14:01:28 +0330

    created ProjectDetails

---
+++ src/app/SampleWork/[id]/page.jsx
@@ -0,0 +1,12 @@
+import Details from '@/components/template/sampleWork/Details/Details'
+import React from 'react'
+
+function ProjectDetails() {
+  return (
+    <>
+    <Details/>
+    </>
+  )
+}
+
+export default ProjectDetails
