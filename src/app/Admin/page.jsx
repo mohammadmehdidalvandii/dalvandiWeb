71652c56d5c08e33875f8ef9ca1b21490e573b6a
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-28 14:20:01 +0330

    created admin-page

---
+++ src/app/Admin/page.jsx
@@ -0,0 +1,12 @@
+import AdminLayout from '@/components/layouts/AdminLayout/AdminLayout'
+import React from 'react'
+
+function Admin() {
+  return (
+    <AdminLayout>
+        this is admin - page
+    </AdminLayout>
+  )
+}
+
+export default Admin
--- src/components/layouts/AdminLayout/AdminLayout.jsx
+++ src/components/layouts/AdminLayout/AdminLayout.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-function AdminLayout({children}) {
+function Layout({children}) {
   return (
     <section className="block w-full">
         <div className="block">
-            <div className="block">this is top bar</div>
             <div className="block">
                 this is sideBar
                 {children}
@@ -14,4 +13,4 @@ function AdminLayout({children}) {
   )
 }
 
-export default AdminLayout
+export default Layout
