c915a646a5f2d87c1119391805c1c59c27aa6181
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-29 14:08:08 +0330

    created aboutMe page for admin-page

---
+++ src/app/Admin/AboutMe/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminLayout from '../../../components/layouts/AdminLayout/AdminLayout'
+
+function page() {
+  return (
+    <AdminLayout>
+        <section className="block mt-8">
+            this aboutMe
+        </section>
+    </AdminLayout>
+  )
+}
+
+export default page
