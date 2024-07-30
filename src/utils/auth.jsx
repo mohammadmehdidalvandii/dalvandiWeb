b12f98da40d93cd21777727679b0fb1db6a37516
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-30 14:55:59 +0330

    created auth for validations password access refresh token

---
+++ src/utils/auth.jsx
@@ -0,0 +1,71 @@
+import { hash , compare } from "bcryptjs";
+import { sign , verify } from "jsonwebtoken";
+
+// Logic Hash Password 
+const hashPassword = async (Password)=>{
+    const hashedPassword = await hash(Password , 12);
+    return hashedPassword
+}
+
+// Logic Verify Password 
+const verifyPassword = async (Password , hashedPassword)=>{
+    const isValid  = await compare(Password , hashedPassword);
+    return isValid;
+}
+
+// Logic Created Access Token 
+const generateAccessToken = (data)=>{
+    const token = sign({...data}, process.evn.ACCESS_TOKEN , {
+        expiresIn : "60d",
+    });
+    return token
+}
+
+// Logic Verify Access Token 
+const verifyAccessToken = (token)=>{
+    try{
+        const tokenPayload = verify(token , process.env.ACCESS_TOKEN);
+        return tokenPayload;
+    } catch (err){
+        console.log("Verify Access Token Error => ", err);
+        return false
+    }
+}
+
+// Logic Created Refresh Token 
+const generateRefreshToken = (data)=>{
+    const token = sign({...data}, process.env.REFRESH_TOKEN , {
+        expiresIn:"15d",
+    });
+    return token;
+}
+
+// Logic Validation Email
+const validEmail = (email)=>{
+    const pattern = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g;
+    return pattern.test(email);
+}
+
+// Logic Validation Phone
+const validPhone = (phone)=>{
+    const pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g;
+    return pattern.test(phone);
+}
+
+// Logic Validation Email
+const validPassword = (Password)=>{
+    const pattern =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
+  return pattern.test(password);
+}
+
+export {
+    hashPassword,
+    verifyPassword,
+    generateAccessToken,
+    verifyAccessToken,
+    generateRefreshToken,
+    validEmail,
+    validPassword,
+    validPhone,
+};
