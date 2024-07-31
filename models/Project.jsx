0f628b6eaca71dcdaa5e1808a6413c0768762f19
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-31 16:52:43 +0330

    created model Prject

---
+++ models/Project.jsx
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    techs:{
+        type:[string],
+        required:true,
+    },
+    github:{
+        type:String,
+        required:true,
+    },
+    onlineLink:{
+        type:String,
+        required:true,
+    }
+});
+
+const model = mongoose.models.Project || mongoose.model("Project" , schema);
+module.exports = model;
