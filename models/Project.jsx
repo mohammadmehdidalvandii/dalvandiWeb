4170772cc7ccbb5a25a4d8919c6eb01ad3141656
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-07 14:49:50 +0330

    created api porject uploads img

--- models/Project.jsx
+++ models/Project.jsx
@@ -5,6 +5,10 @@ const schema = new mongoose.Schema({
         type:String,
         required:true,
     },
+    img:{
+        type:String,
+        required:true,
+    },
     description:{
         type:String,
         required:true,
--- src/app/api/projects/route.jsx
+++ src/app/api/projects/route.jsx
@@ -1,36 +1,61 @@
 import connectToDB from "@/config/db";
-import ProjectModel from '@/models/Project';
+import ProjectModel from "@/models/Project";
+import Path from 'path';
+import {writeFile } from "fs/promises"
 
-export async function POST (req) {
-    try{
-        connectToDB();
-        const body = await req.json();
-        const {
-            title,
-            description,
-            techs,
-            github,
-            onlineLink,
-        } = body;
 
-        const project = await  ProjectModel.create({
-            title,
-            description,
-            techs,
-            github,
-            onlineLink,
-        });
+export async function POST(req) {
+  try {
+    connectToDB();
+    const formData = await req.formData();
 
-        return Response.json(
-            {message :"Project Created Successfully ✅" , data:project},
-            {status:"201"}
-        )
+    const title = formData.get("title");
+    const img = formData.get("img");
+    const description = formData.get("description");
+    const techs = formData.get("techs");
+    const github = formData.get("github");
+    const onlineLink = formData.get("onlineLink");
 
-    } catch(err){
-    console.log("error=>>" ,err)
-        return Response.json(
-            {message:"Error Projects Server =>" , err},
-            {status:500}
-        );
-    }
-}
+    // generate Buffer for img  
+    const buffer = Buffer.from(await img.arrayBuffer());
+    const fileName = Date.now() + img.name;
+    const imgPath = Path.join(process.cwd() , "public/uploads/" + fileName);
+
+    await writeFile(imgPath , buffer);
+
+    const project = await ProjectModel.create({
+        title,
+        img:`http://localhost:3000/uploads/${fileName}`,
+        description,
+        techs,
+        github,
+        onlineLink,
+    });
+
+    return Response.json(
+        {message : "Created Project is Successfully ✅" , data:project},
+        {status:201}
+    )
+
+  } catch (err) {
+    console.log("error=>>", err);
+    return Response.json(
+      { message: "Error Projects Server =>", err },
+      { status: 500 }
+    );
+  }
+} 
+
+// // Image Uploader
+// export async function PUT(req) {
+//     const formData = await req.formData();
+//     const img = formData.get("img");
+  
+//     // Validation
+//     if (!img) {
+//       return Response.json(
+//         { message: "Product has not image !!" },
+//         { status: 400 }
+//       );
+//     }
+// }
