f79d1330854a641a0e0b7ad2542ceeec469c8736
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-08-08 14:33:33 +0330

    add length message and project

--- src/app/Admin/page.jsx
+++ src/app/Admin/page.jsx
@@ -1,14 +1,17 @@
 import AdminLayout from '@/components/layouts/AdminLayout/AdminLayout'
 import Box from '@/components/template/admin/Box/Box'
 import React from 'react'
+import MessageModel from '@/models/Messages';
+import ProjectModel from "@/models/Project";
 
-function Admin() {
+async function Admin() {
+  const messages = await MessageModel.find({})
+  const projects = await ProjectModel.find({})
   return (
     <AdminLayout>
         <div className="flex items-center gap-6  mt-8">
-            <Box title='تعداد کل در خواست ها' number="12"/>
-            <Box title='تعداد پروژه ها' number="12"/>
-            <Box title='درخواست های امروز' number="12"/>
+            <Box title='تعداد کل در خواست ها' number={messages.length}/>
+            <Box title='تعداد پروژه ها' number={projects.length}/>
         </div>
     </AdminLayout>
   )
